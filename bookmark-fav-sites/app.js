45d3d3a8d26381e909313f9ada6fe94f3e127e54
anisur2805 <anisur2805@gmail.com>
2020-02-09 01:27:01 +0600

    starting bookmark site

---
+++ bookmark-fav-sites/app.js
@@ -0,0 +1,29 @@
+document.getElementById("myForm").addEventListener("submit", saveBookmark);
+
+function saveBookmark(event) {
+  event.preventDefault();
+
+  const siteName = document.querySelector("#siteName").value;
+  const siteUrl = document.querySelector("#siteUrl").value;
+
+  console.log(siteName);
+
+  const bookmark = {
+    name: siteName,
+    url: siteUrl
+  };
+
+  if (localStorage.getItem("bookmarks") === null) {
+    const bookmarks = [];
+    bookmarks.push(bookmark);
+    localStorage.setItem("bookmarks", JSON.stringify(bookmarks));
+  } else {      
+      const bookmarks = JSON.parse(localStorage.getItem('bookmarks'))
+      bookmarks.push(bookmark)
+      localStorage.setItem("bookmarks", JSON.stringify(bookmarks));
+  }
+  localStorage.setItem("Test", "Hello World!");
+}
+
+
+https://youtu.be/DIVfDZZeGxM?list=PLillGF-RfqbbnEGy3ROiLWk7JMCuSyQtX&t=1483
