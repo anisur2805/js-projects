59fb487a078807c922fab2948307a462b24e0029
anisur2805 <anisur2805@gmail.com>
2020-02-16 01:23:02 +0600

    done bookmarks app, alhamdulillah

--- bookmark-fav-sites/app.js
+++ bookmark-fav-sites/app.js
@@ -6,7 +6,11 @@ function saveBookmark(event) {
   const siteName = document.querySelector("#siteName").value;
   const siteUrl = document.querySelector("#siteUrl").value;
 
-  console.log(siteName);
+  if (!validateForm(siteName, siteUrl)) {
+    return false;
+  }
+
+  // console.log(siteName);
 
   const bookmark = {
     name: siteName,
@@ -17,10 +21,74 @@ function saveBookmark(event) {
     const bookmarks = [];
     bookmarks.push(bookmark);
     localStorage.setItem("bookmarks", JSON.stringify(bookmarks));
-  } else {      
-      const bookmarks = JSON.parse(localStorage.getItem('bookmarks'))
-      bookmarks.push(bookmark)
-      localStorage.setItem("bookmarks", JSON.stringify(bookmarks));
+  } else {
+    const bookmarks = JSON.parse(localStorage.getItem("bookmarks"));
+    bookmarks.push(bookmark);
+    localStorage.setItem("bookmarks", JSON.stringify(bookmarks));
   }
-  localStorage.setItem("Test", "Hello World!");
+  
+  document.getElementById('myForm').reset()
+
+  // Re-fetch Bookmarks
+  fetchBookmarks();
+}
+
+function deleteBookmark(url) {
+  const bookmarks = JSON.parse(localStorage.getItem("bookmarks"));
+
+  for (let i = 0; i < bookmarks.length; i++) {
+    if (bookmarks[i].url == url) {
+      bookmarks.splice(i, 1);
+    }
+  }
+
+  // Reset LocalStorage
+  localStorage.setItem("bookmarks", JSON.stringify(bookmarks));
+
+  // Re-fetch Bookmarks
+  fetchBookmarks();
+}
+
+function fetchBookmarks() {
+  const bookmarks = JSON.parse(localStorage.getItem("bookmarks"));
+
+  const bookmarksResult = document.getElementById("bookmarksResults");
+
+  bookmarksResult.innerHTML = "";
+  for (let i = 0; i < bookmarks.length; i++) {
+    let name = bookmarks[i].name;
+    let url = bookmarks[i].url;
+
+    bookmarksResult.innerHTML += `    
+    <div class="card card-body bg-light mb-2">
+      <h3>${name}</h3>
+      <div class="btn-group" role="group">
+      <a class="btn btn-primary" href="${url}" target="_blank">Visite</a>
+      <a onclick="deleteBookmark('${url}')" class="btn btn-danger" href="#">Delete</a>
+      </div>
+    </div>
+    `;
+
+    // bookmarksResult.innerHTML += '<div class="card card-body bg-light mb-2">'+
+    // '<h3>'+name+'</h3>' +
+    // '<a onclick="deleteBookmark(\''+url+'\')" href="">Delete</a>'+
+    // '</div>'
+  }
+}
+
+function validateForm(siteName, siteUrl) {
+  if (!siteName || !siteUrl) {
+    alert("Fill the all field");
+    return false;
+  }
+
+  var expression = /[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)?/gi;
+  var regex = new RegExp(expression);
+
+  if (!siteUrl.match(regex)) {
+    alert("Please use a valid URL");
+    return false;
+  }
+
+  return true;
 }
