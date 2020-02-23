297783a2df3f33b7124707ab2d718151e38e608f
anisur2805 <anisur2805@gmail.com>
2020-02-23 23:40:22 +0600

    started geocode api

--- geocode-api/app.js
+++ geocode-api/app.js
@@ -1,17 +1,20 @@
 geocode();
 function geocode() {
-                  var location = "22 Main st Boston MA"
-                  axios.get('https://maps.googleapis.com/maps/api/geocode/json', {
-                                    params: {
-                                                      address: location,
-                                                      key: 'AIzaSyBVnVZtGcKOCoSFNRU42OuB4sWBDHvmRM8'
-                                    }
-                  })
-                                    .then(function (response) {
-                                                      console.log(response);
-                                    })
-                                    .catch(function (error) {
-                                                      console.log(error);
-                                    });
-
+  var location = "22 Main st Boston MA";
+  axios
+    .get("https://maps.googleapis.com/maps/api/geocode/json", {
+      params: {
+        address: location,
+        key: "AIzaSyBO59mo6rMe4ChzmBqEQgz9QmWjg_X38c"
+      }
+    })
+    .then(function(response) {
+      console.log(response);
+    })
+    .catch(function(error) {
+      console.log(error);
+    });
 }
+
+
+https://youtu.be/pRiQeo17u6c?list=PLillGF-RfqbbnEGy3ROiLWk7JMCuSyQtX&t=682
